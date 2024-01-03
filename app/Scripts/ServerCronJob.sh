f3a4b6047978bea4cba760c28693ffc832477320
Armin Eslami <armineslami@hotmail.com>
2024-01-03 19:47:23 +0330

    Fix project directory path for cron job

--- app/Scripts/ServerCronJob.sh
+++ app/Scripts/ServerCronJob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-project_path="/var/html/ssh-accounting-panel"
+project_path="/var/www/sap"
 
 (crontab -l ; echo "* * * * * cd $project_path && php artisan schedule:run >> /dev/null 2>&1") | crontab -
