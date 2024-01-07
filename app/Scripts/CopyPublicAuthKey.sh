687fd69e3e5ea116405a28fc0d0e5339d21c1f37
Armin Eslami <armineslami@hotmail.com>
2024-01-07 12:59:27 +0330

    Update the path to ssh keys.

--- app/Scripts/CopyPublicAuthKey.sh
+++ app/Scripts/CopyPublicAuthKey.sh
@@ -6,22 +6,17 @@ createResponse() {
     echo "$json_string"
 }
 
-if [[ -z $1 ]]; then
-    # Copy the key to the authorized_keys file of the local machine
-    if [ ! -f /root/.ssh/authorized_keys ]; then
-        mkdir -p /root/.shh
-        touch /root/.ssh/authorized_keys
-        chmod 700 ~/.ssh
-        chmod 600 ~/.ssh/authorized_keys
-    fi
-    cat /root/.ssh/ssh_accounting_panel.pub >> /root/.ssh/authorized_keys
+if [[ -n $1 ]]; then
+    # Remove any old entry for this host in know_hosts file
+    ssh-keygen -f ~/.ssh/known_hosts -R "[$3]:$4"  > /dev/null 2>&1
 
-    createResponse "1" "successful"
-    exit 1
-else
+    cd ../storage/keys
+
     # Copy the key to the server
-    sshpass -p "$2" ssh-copy-id -p "$4" -i /root/.ssh/ssh_accounting_panel.pub "$1"@"$3" > /dev/null 2>&1
+    cat ssh_accounting_panel.pub | sshpass -p "$2" ssh -o StrictHostKeyChecking=no -p "$4" "$1"@"$3" "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" > /dev/null 2>&1
 
     # Test if SSH connection works with the key
-    ssh -q -o BatchMode=yes -o ConnectTimeout=5 -i /root/.ssh/ssh_accounting_panel -p "$4" "$1"@"$3" 'echo Connected' >/dev/null 2>&1 && (createResponse "1" "Successful") || (createResponse "0" "Failed to connect to the server")
+    ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no -o ConnectTimeout=5 -i ssh_accounting_panel -p "$4" "$1"@"$3" 'echo Connected' >/dev/null 2>&1 && (createResponse "1" "Successful") || (createResponse "0" "Failed to connect to the server")
+else
+    createResponse "0" "Server address is required"
 fi
