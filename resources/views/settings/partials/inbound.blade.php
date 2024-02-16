966ae6884372bb86ab5bb98bf83cf0cfc3e6a27a
Armin Eslami <armineslami@hotmail.com>
2024-02-16 22:36:23 +0330

    Set inputs type to number

--- resources/views/settings/partials/inbound.blade.php
+++ resources/views/settings/partials/inbound.blade.php
@@ -20,7 +20,7 @@
                     <x-input-label for="inbound_traffic_limit"
                                    :value="__('*Traffic Limit (GB, Blank = ∞)')"/>
                     <x-text-input id="inbound_traffic_limit" name="inbound_traffic_limit" placeholder="traffic limit e.g. 100"
-                                  type="text"
+                                  type="number"
                                   class="mt-1 block w-full"
                                   :value="old('inbound_traffic_limit', $settings->inbound_traffic_limit)" />
                     <x-input-error id="inbound_traffic_limit_error" class="mt-2" :messages="$errors->get('inbound_traffic_limit')"/>
@@ -28,7 +28,7 @@
 
                 <div>
                     <x-input-label for="inbound_max_login" :value="__('*Max Login')"/>
-                    <x-text-input id="inbound_max_login" name="inbound_max_login" placeholder="max login e.g. 1" type="text"
+                    <x-text-input id="inbound_max_login" name="inbound_max_login" placeholder="max login e.g. 1" type="number"
                                   class="mt-1 block w-full"
                                   :value="old('inbound_max_login', $settings->inbound_max_login)" required />
                     <x-input-error id="inbound_max_login_error" class="mt-2" :messages="$errors->get('inbound_max_login')"/>
@@ -37,7 +37,7 @@
                 <div>
                     <x-input-label for="inbound_active_days" :value="__('*Active Days (Blank = ∞)')"/>
                     <x-text-input id="inbound_active_days" name="inbound_active_days" placeholder="active days e.g. 30"
-                                  type="text"
+                                  type="number"
                                   class="mt-1 block w-full"
                                   :value="old('inbound_active_days', $settings->inbound_active_days)" />
                     <x-input-error id="inbound_active_days_error" class="mt-2" :messages="$errors->get('inbound_active_days')"/>
