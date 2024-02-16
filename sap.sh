6039606a575b3eabcff81f9716becda7ac8c8a21
Armin Eslami <armineslami@hotmail.com>
2024-02-16 19:40:21 +0330

    Fix bugs

--- sap.sh
+++ sap.sh
@@ -424,7 +424,7 @@ install() {
     ### Laravel Setup ###
     #####################
 
-    printf "${GREEN}\nSetting up the framework ...${NC}"
+    printf "${GREEN}\nSetting up the framework ...\n${NC}"
 
     domain=""
 
