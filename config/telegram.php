91f651941c1058d851a7a9ca2134e57c06228d17
Armin Eslami <armineslami@hotmail.com>
2024-02-22 09:25:27 +0330

    Fix a bug resulting 'A facade root has not been set'

--- config/telegram.php
+++ config/telegram.php
@@ -84,7 +84,7 @@ return [
     | Default: GuzzlePHP
     |
     */
-    'http_client_handler' => env("APP_ENV", "") === "local" && !App::runningInConsole() ? new ProxyHttpClient() : null,
+    'http_client_handler' => env("APP_ENV", "") === "local" ? new ProxyHttpClient() : null,
 
     /*
     |--------------------------------------------------------------------------
